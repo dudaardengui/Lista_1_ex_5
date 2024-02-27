f8f086d162ae5a57f65130183590e6df3a40c043
ALUNO <ALUNO@LABHPC06>
2024-02-27 15:01:56 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            Console.WriteLine("Digite um valor:");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor:");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor:");
+            C = int.Parse(Console.ReadLine());
+
+            if (A == B & B == C)
+            {
+                Console.WriteLine("Triângulo Equilátero");
+            }
+            else if (A == B || B == C || A == C)
+            {
+                Console.WriteLine("Triângulo Isósceles");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Escaleno");
+            }
+            Console.ReadKey();
+        }
+    }
+}
